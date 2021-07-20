03d746738ce1d8ac16de1ff8d5deba58a38db717
huynxtb <huynx.dev.dn@gmail.com>
2021-07-20 08:17:33 +0700

    new code

---
+++ GroupProject/app/src/main/java/edu/fpt/groupproject/InboxActivity.java
@@ -0,0 +1,14 @@
+package edu.fpt.groupproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InboxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inbox_layout);
+    }
+}
