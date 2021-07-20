62e46cc39bf4cd4217836ac0789a4a750fd85c6e
Hien Trinh <nhochiennhoc@gmail.com>
2021-07-20 22:22:18 +0700

    home, search page, room detail

---
+++ GroupProject/app/src/main/java/edu/fpt/groupproject/model/Room.java
@@ -0,0 +1,115 @@
+package edu.fpt.groupproject.model;
+
+public class Room {
+    protected int id;
+    protected String author;
+    protected String title;
+    protected String content;
+    protected double price;
+    protected String type;
+    protected String image;
+    protected double electricity;
+    protected double water;
+    protected double wifi;
+    protected String note;
+    protected String time;
+
+    public Room() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getElectricity() {
+        return electricity;
+    }
+
+    public void setElectricity(double electricity) {
+        this.electricity = electricity;
+    }
+
+    public double getWater() {
+        return water;
+    }
+
+    public void setWater(double water) {
+        this.water = water;
+    }
+
+    public double getWifi() {
+        return wifi;
+    }
+
+    public void setWifi(double wifi) {
+        this.wifi = wifi;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
